c0484e4e63353e30a9a0015e3ebeb4dc12a9e728
luian alves <luian.alves@gmail.com>
2025-08-06 07:41:19 -0300

    Ajustando view de account

--- resources/views/components/form.blade.php
+++ resources/views/components/form.blade.php
@@ -2,4 +2,8 @@
     @csrf
 
     @include("$path")
+
+    <div class="text-end mt-3">
+        <button type="submit" class="btn btn-sm bg-color">Salvar</button>
+    </div>
 </form>
--- resources/views/components/input-check.blade.php
+++ resources/views/components/input-check.blade.php
@@ -1,7 +1,6 @@
 <div class="col-{{ $col }} offset-{{ $set }}">
-    <div class="form-check form-switch">
-        <input class="form-check-input" type="checkbox" name="{{ $name }}" id="{{ $id }}"
-            {{ $checked ? 'checked=""' : '' }} {{ $disabled ? 'disabled' : '' }}>
-        <label class="form-check-label" for="{{ $id }}">{{ $title }}</label>
+    <div class="form-check">
+        <input class="form-check-input" type="radio" name="{{ $name }}" id="{{$id}}" {{ $checked ? 'checked=""' : '' }} {{ $disabled ? 'disabled' : '' }}>
+        <label class="form-check-label" for="{{$id}}">{{ $title }}</label>
     </div>
 </div>

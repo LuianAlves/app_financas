7363a0f9aeeaef64702bdd47d05ff5dba499c20a
LuianAlves <luian.alves@gmail>
2025-08-07 16:17:38 -0300

    #

--- resources/views/layouts/partials/head.blade.php
+++ resources/views/layouts/partials/head.blade.php
@@ -1,46 +1,23 @@
 <head>
-    <meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="csrf-token" content="{{ csrf_token() }}">
 
-    <title>Finanças pessoal</title>
+    <title>Financial App UI</title>
 
-    <link rel="apple-touch-icon" sizes="180x180" href="{{asset('assets/img/favicons/apple-touch-icon.png')}}">
-    <link rel="icon" type="image/png" sizes="32x32" href="{{asset('assets/img/favicons/favicon-32x32.png')}}">
-    <link rel="icon" type="image/png" sizes="16x16" href="{{asset('assets/img/favicons/favicon-16x16.png')}}">
-    <link rel="shortcut icon" type="image/x-icon" href="{{asset('assets/img/favicons/favicon.ico')}}">
-    <link rel="manifest" href="{{asset('assets/img/favicons/manifest.json')}}">
-    <meta name="msapplication-TileImage" content="{{asset('assets/img/favicons/mstile-150x150.png')}}">
-    <meta name="theme-color" content="#ffffff">
-    <script src="{{asset('assets/js/config.js')}}"></script>
-    <script src="{{asset('assets/vendors/simplebar/simplebar.min.js')}}"></script>
+    <link rel="manifest" href="{{ asset('laravelpwa/manifest.json') }}">
+    <meta name="theme-color" content="#00bfa6">
 
-    <link rel="preconnect" href="https://fonts.gstatic.com">
-    <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,400,500,600,700%7cPoppins:300,400,500,600,700,800,900&amp;display=swap" rel="stylesheet">
+    <!-- Bootstrap -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
 
-    <script src="https://kit.fontawesome.com/d949d91b0e.js" crossorigin="anonymous"></script>
+    <!-- Font Awesome -->
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.0/css/all.min.css" rel="stylesheet">
 
-    <link href="{{asset('assets/vendors/simplebar/simplebar.min.css')}}" rel="stylesheet">
-    <link href="{{asset('assets/css/theme-rtl.css')}}" rel="stylesheet" id="style-rtl">
-    <link href="{{asset('assets/css/theme.css')}}" rel="stylesheet" id="style-default">
-    <link href="{{asset('assets/css/user-rtl.css')}}" rel="stylesheet" id="user-style-rtl">
-    <link href="{{asset('assets/css/user.css')}}" rel="stylesheet" id="user-style-default">
+    <!-- Flatpickr -->
+    <link href="https://cdn.jsdelivr.net/npm/flatpickr/dist/flatpickr.min.css" rel="stylesheet">
 
-    <script>
-        var isRTL = JSON.parse(localStorage.getItem('isRTL'));
-        if (isRTL) {
-            var linkDefault = document.getElementById('style-default');
-            var userLinkDefault = document.getElementById('user-style-default');
-            linkDefault.setAttribute('disabled', true);
-            userLinkDefault.setAttribute('disabled', true);
-            document.querySelector('html').setAttribute('dir', 'rtl');
-        } else {
-            var linkRTL = document.getElementById('style-rtl');
-            var userLinkRTL = document.getElementById('user-style-rtl');
-            linkRTL.setAttribute('disabled', true);
-            userLinkRTL.setAttribute('disabled', true);
-        }
-    </script>
+    @stack('styles')
 
-    <link href="{{asset('assets/css/style.css')}}" rel="stylesheet">
+    <link href="{{ asset('assets/css/style.css') }}" rel="stylesheet">
 </head>

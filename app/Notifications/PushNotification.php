945df86177b16cf72d26db8d49dc10d225677aaf
LuianAlves <luian.alves@gmail>
2025-08-26 10:46:46 -0300

    Ajustes pushs

--- app/Notifications/PushNotification.php
+++ app/Notifications/PushNotification.php
@@ -32,7 +32,7 @@ class PushNotification extends Notification
             ->title($this->title)
             ->body($this->body)
             ->icon('/laravelpwa/icons/icon-192x192.png')
-            ->action('Abrir app', '/')
-            ->data(['url' => '/']);
+            ->action('Abrir app', '/transactions')
+            ->data(['url' => '/transactions']);
     }
 }

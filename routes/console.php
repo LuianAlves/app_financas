7a6e0eab2fe4ac62522e1373fb0d6aa7e4aa09d1
LuianAlves <luian.alves@gmail>
2025-08-26 15:52:04 -0300

    Ajustes pushs

--- routes/console.php
+++ routes/console.php
@@ -7,6 +7,6 @@ use App\Jobs\ProcessRecurrences;
 Schedule::job(new ProcessRecurrences)->dailyAt('03:10');
 Schedule::job(new SendDailyDigestJob)
     ->timezone('America/Sao_Paulo')
-    ->dailyAt('15:45');
+    ->dailyAt('15:55');
 
-Schedule::job(new SendDailyDigestJob)->dailyAt('15:50');
+Schedule::job(new SendDailyDigestJob)->dailyAt('15:53');

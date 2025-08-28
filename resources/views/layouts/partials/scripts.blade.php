5a4bc5495b1f412135411a7473262207a481acbf
luian alves <luian.alves@gmail.com>
2025-08-28 16:25:51 -0300

    Ajustando botão install

--- resources/views/layouts/partials/scripts.blade.php
+++ resources/views/layouts/partials/scripts.blade.php
@@ -1,24 +1,24 @@
-{{--<script>--}}
-{{--    (function(){--}}
-{{--        const ua = navigator.userAgent || '';--}}
-{{--        const isIOSLike =--}}
-{{--            (/(iPad|iPhone|iPod)/.test(ua) && !window.MSStream) ||--}}
-{{--            (navigator.platform === 'MacIntel' && (navigator.maxTouchPoints || 0) > 1);--}}
+<script>
+    (function(){
+        const ua = navigator.userAgent || '';
+        const isIOSLike =
+            (/(iPad|iPhone|iPod)/.test(ua) && !window.MSStream) ||
+            (navigator.platform === 'MacIntel' && (navigator.maxTouchPoints || 0) > 1);
 
-{{--        window.AUTH = @json(auth()->check());--}}
-{{--        window.PUSH_CFG = {--}}
-{{--            vapidKeyUrl: "{{ url('/vapid-public-key') }}",--}}
-{{--            subscribeUrl: "{{ url('/push/subscribe') }}",--}}
-{{--            swUrl: "{{ asset('sw.js') }}?v={{ filemtime(public_path('sw.js')) }}",--}}
-{{--            loginPath: "{{ route('login') }}",--}}
-{{--            isIOS: isIOSLike--}}
-{{--        };--}}
-{{--    })();--}}
-{{--</script>--}}
+        window.AUTH = @json(auth()->check());
+        window.PUSH_CFG = {
+            vapidKeyUrl: "{{ url('/vapid-public-key') }}",
+            subscribeUrl: "{{ url('/push/subscribe') }}",
+            swUrl: "{{ asset('sw.js') }}?v={{ filemtime(public_path('sw.js')) }}",
+            loginPath: "{{ route('login') }}",
+            isIOS: isIOSLike
+        };
+    })();
+</script>
 
-{{--@php--}}
-{{--    $pushRegisterPath = public_path('assets/js/push-register.js');--}}
-{{--@endphp--}}
-{{--<script src="{{ asset('assets/js/push-register.js') }}?v={{ file_exists($pushRegisterPath) ? filemtime($pushRegisterPath) : time() }}" defer></script>--}}
+@php
+    $pushRegisterPath = public_path('assets/js/push-register.js');
+@endphp
+<script src="{{ asset('assets/js/push-register.js') }}?v={{ file_exists($pushRegisterPath) ? filemtime($pushRegisterPath) : time() }}" defer></script>
 
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" crossorigin="anonymous"></script>

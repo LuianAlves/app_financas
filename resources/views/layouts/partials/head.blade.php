f960f68122e90a0fb63b7bd7d2781cc72d6b53fc
luian alves <luian.alves@gmail.com>
2025-09-06 07:30:02 -0300

    Ajustes layout

--- resources/views/layouts/partials/head.blade.php
+++ resources/views/layouts/partials/head.blade.php
@@ -3,9 +3,9 @@
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
 {{--    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no">--}}
-{{--    <meta name="mobile-web-app-capable" content="yes">--}}
-{{--    <meta name="apple-mobile-web-app-capable" content="yes">--}}
-{{--    <meta name="apple-mobile-web-app-status-bar-style" content="black-translucent">--}}
+    <meta name="mobile-web-app-capable" content="yes">
+    <meta name="apple-mobile-web-app-capable" content="yes">
+    <meta name="apple-mobile-web-app-status-bar-style" content="black-translucent">
 
     <title>Finanças Cliqis</title>
 

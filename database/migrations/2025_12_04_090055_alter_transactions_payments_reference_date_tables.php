4eedd0522c2fb0e056d9efd5009adc10c6176951
Luian Alves <60886829+LuianAlves@users.noreply.github.com>
2025-12-04 05:31:25 -0300

    Atualizar o 2025_12_04_090055_alter_transactions_payments_reference_date_tables.php

--- database/migrations/2025_12_04_090055_alter_transactions_payments_reference_date_tables.php
+++ database/migrations/2025_12_04_090055_alter_transactions_payments_reference_date_tables.php
@@ -1,4 +1,26 @@
-// migration
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Support\Facades\DB;
+
+return new class extends Migration
+{
+    public function up(): void
+    {
+        // migration
 Schema::table('payment_transactions', function (\Illuminate\Database\Schema\Blueprint $table) {
     $table->date('reference_date')->nullable()->after('payment_date');
 });
+        
+    }
+
+    public function down(): void
+    {
+        Schema::table('payment_transactions', function (Blueprint $table) {
+            
+        });
+    }
+
+};
